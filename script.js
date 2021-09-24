b6fb55aeb719bfe6f16c8b1e62ab2f062f3a01a0
Mayckon Rebecci <mayckon_mmr@hotmail.com>
2021-09-24 00:39:33 -0300

    adding features

--- script.js
+++ script.js
@@ -4,27 +4,75 @@ let number = document.querySelector(".number");
 let inputGuess = document.querySelector(".guess");
 let btnCheck = document.querySelector(".check");
 let btnAgain = document.querySelector(".again");
+let btnReset = document.querySelector(".reset");
+let message = document.querySelector(".message");
+let bodyBG = document.querySelector("body");
+let score = document.querySelector(".score");
+let highScore = document.querySelector(".highscore");
 
-number = Math.floor(Math.random() * 6) + 1;
+number = Math.floor(Math.random() * 20) + 1;
+
+// ------------------------------
+// ! ARRUMAR
+// ------------------------------
+if (highScore.innerHTML = 0) {
+    btnReset.setAttribute('disabled');
+}
 
 let alertGuess = () => {
     alert(number);
 }
 
 let clickCheck = () => {
-    btnCheck.getAttribute("click", alertGuess())
+    btnCheck.getAttribute("click", alertGuess());
 
     if(Number(inputGuess.value) === number) {
-        alert("acerto mizeravi");
+        
+        message.innerText = "🎉 Correct Number!"
+
+        bodyBG.style.backgroundColor = "#60b347";
+        
+        if (score.innerHTML > highScore.innerHTML) {
+            highScore.innerHTML = score.innerHTML;            
+        }
+        
     }
     else {
-        alert("erro mizeravi");
+        if (Number(inputGuess.value) > number) {
+            message.innerText = "📈 Too High!";
+
+            score.innerHTML--;
+        }
+        if (Number(inputGuess.value) < number) {
+            message.innerText = "📉 Too Low!";
+
+            score.innerHTML--;
+        }
     }
 
-    inputGuess.value = 0;
+    inputGuess.value = 1;
 }
 
 let clickAgain = () => {
 
-    number = Math.floor(Math.random() * 6) + 1;
+    number = Math.floor(Math.random() * 20) + 1;
+
+    bodyBG.style.backgroundColor = "";
+
+    score.innerHTML = 20;
+}
+
+let clickReset = () => {
+
+    if (highScore.innerHTML === 0) {
+        btnReset.setAttribute("disable");
+    }
+
+    highScore.innerHTML = 0;
+
+    number = Math.floor(Math.random() * 20) + 1;
+
+    bodyBG.style.backgroundColor = "";
+
+    score.innerHTML = 20;
 }
